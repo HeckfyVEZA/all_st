3d618f700e5c5939ec0e770a37a800816855e23f
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-07-24 11:40:29 +0300

    Update all_st.py

--- all_st.py
+++ all_st.py
@@ -193,7 +193,7 @@ with col[{i}][{j}]:\n
                 checklist[syss].append(None)
     st.download_button(label='💾 Скачать файл для выгрузки в КП',data=to_excel(fintable) ,file_name= 'для кп.xls')
     
-    st.download_button(label='Разбивка для кп по системам',data=to_excel(all_inf), file_name='NNV.xls')
+    st.download_button(label='Разбивка для кп по системам',data=to_excel(DataFrame(all_inf)), file_name='NNV.xls')
     # st.write([len(checklist[chk]) for chk in checklist.keys()])
     try:
         st.download_button(label='💾 Скачать проверочный файл',data=to_excel(DataFrame(checklist), HEADER=True, START=0) ,file_name= 'проверка.xlsx')
